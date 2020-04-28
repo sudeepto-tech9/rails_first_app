112408ef7cc77c606fb725718fce9585cdfd2094
Sudeepto Dutta <sudeepto-dutta@outlook.com>
2020-04-28 16:25:15 +0530

    Hello World with pages controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,3 +7,22 @@ end
 # We write/generate logic based code to dictate how the user handles the response , request &
 # routing within the app.
 # For a 'Users' resource Model, we might a corresponding 'users_controlloer' Controller file
+
+# MVC - Model, View, Controller
+
+# General flow of Rails application:
+
+# -> Request made at browser
+
+# -> Request received at router of rails application
+
+# -> Request routed to appropriate action in a controller
+
+# -> Controller#action either renders a view template or communicates with model
+
+# -> Model communicates with database
+
+# -> Model sends back information to controller
+
+# -> Controller renders view
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
 end
+
+# Conventional Expectations
+
+# Define a route that points to a controller#action
+# Have an appropriately named controller for .e.g, if dealing with layouts of static pages of the application,
+# a name could be pages_controller
+# Have an appriately named action, for e.g if dealing with with a homepage, the action/method name could be home.
