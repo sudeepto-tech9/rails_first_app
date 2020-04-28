342062d96ac62265e16c736a44715cd3c3067740
Sudeepto Dutta <sudeepto-dutta@outlook.com>
2020-04-28 14:44:14 +0530

    Added MVC related intro points.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
 end
+
+# Controllers are the 'C' in 'MVC' .
+# They are invisible to the user.
+# Controllers are called the brains behind the Ruby application.
+# We write/generate logic based code to dictate how the user handles the response , request &
+# routing within the app.
+# For a 'Users' resource Model, we might a corresponding 'users_controlloer' Controller file
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+# Models are resources used  the app ; Users, Post, Article, Stock.
+# Models usually require a persistence or database layer.
+# In Rails, we use the Models to communicate with the tables in the database.
+#
